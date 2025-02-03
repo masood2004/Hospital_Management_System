cef53130d3e4d7acc6b5f0ced76ea7c2262c1b4b
Syed Masood Hussain <hmasood3288@gmail.com>
2025-02-04 00:38:47 +0500

    Create Checkup Class

---
+++ src/main/java/com/mycompany/hospital_management_system/Checkup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hospital_management_system;
+
+/**
+ *
+ * @author hmaso
+ */
+public class Checkup {
+    Doctor Doctor;
+    Patient Patient;
+    int Priority;
+    String Recommandation, Date;
+}
