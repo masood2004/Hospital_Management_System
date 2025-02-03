2dd12e0e44d3a556fac17ed451829d636f815d82
Syed Masood Hussain <hmasood3288@gmail.com>
2025-02-04 00:41:57 +0500

    Initialize variables, create constructors, getters and setters, and toString in Checkup Class

--- src/main/java/com/mycompany/hospital_management_system/Checkup.java
+++ src/main/java/com/mycompany/hospital_management_system/Checkup.java
@@ -13,4 +13,60 @@ public class Checkup {
     Patient Patient;
     int Priority;
     String Recommandation, Date;
+
+    public Checkup(Doctor Doctor, Patient Patient, int Priority, String Recommandation, String Date) {
+        this.Doctor = Doctor;
+        this.Patient = Patient;
+        this.Priority = Priority;
+        this.Recommandation = Recommandation;
+        this.Date = Date;
+    }
+
+    public Doctor getDoctor() {
+        return Doctor;
+    }
+
+    public void setDoctor(Doctor Doctor) {
+        this.Doctor = Doctor;
+    }
+
+    public Patient getPatient() {
+        return Patient;
+    }
+
+    public void setPatient(Patient Patient) {
+        this.Patient = Patient;
+    }
+
+    public int getPriority() {
+        return Priority;
+    }
+
+    public void setPriority(int Priority) {
+        this.Priority = Priority;
+    }
+
+    public String getRecommandation() {
+        return Recommandation;
+    }
+
+    public void setRecommandation(String Recommandation) {
+        this.Recommandation = Recommandation;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    @Override
+    public String toString() {
+        return "Checkup{" + "Doctor=" + Doctor.toString() + ", Patient=" + Patient.toString() + ", Priority=" + Priority + ", Recommandation=" + Recommandation + ", Date=" + Date + '}';
+    }
+    
+    
+    
 }
