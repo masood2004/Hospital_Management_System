0726daa73119c03d5355224083598a86059cb6d0
Syed Masood Hussain <hmasood3288@gmail.com>
2025-02-04 00:35:45 +0500

    Initialize variables, create constructors, getters and setters, and toString

--- src/main/java/com/mycompany/hospital_management_system/Doctor.java
+++ src/main/java/com/mycompany/hospital_management_system/Doctor.java
@@ -9,5 +9,66 @@ package com.mycompany.hospital_management_system;
  * @author hmaso
  */
 public class Doctor {
+    private String Id, Name, Contact, Specialty;
+    private int fees;
+
+    public Doctor() {
+    }
+
+    
+    
+    public Doctor(String Id, String Name, String Contact, String Specialty, int fees) {
+        this.Id = Id;
+        this.Name = Name;
+        this.Contact = Contact;
+        this.Specialty = Specialty;
+        this.fees = fees;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public void setContact(String Contact) {
+        this.Contact = Contact;
+    }
+
+    public String getSpecialty() {
+        return Specialty;
+    }
+
+    public void setSpecialty(String Specialty) {
+        this.Specialty = Specialty;
+    }
+
+    public int getFees() {
+        return fees;
+    }
+
+    public void setFees(int fees) {
+        this.fees = fees;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" + "Id=" + Id + ", Name=" + Name + ", Contact=" + Contact + ", Specialty=" + Specialty + ", fees=" + fees + '}';
+    }
+    
     
 }
